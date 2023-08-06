b8fce0e4aba7922f4d3b67ee660eda96c1cc4e74
GosuCode <gosucode1945@gmail.com>
2023-08-06 20:03:34 +0545

    list of users and latest post with number of users and posts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,31 +1,47 @@
 import { useEffect, useState } from "react";
-import UserChart from "../components/charts/UserChart";
 import axios from "axios";
+import moment from 'moment'
 
 const Dashboard = () => {
+  const [blog, setBlog] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/auth/listOfUsers");
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/posts");
+      setBlog(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    fetchUser();
+  }, [])
+
+  const formatCreatedAt = (timestamp) => {
+    return moment(timestamp).format("MMMM Do YYYY");
+  };
+
 
   const stats = [
-    { Name: "Users", number: `12` },
-    { Name: "Post", number: "123" },
-    { Name: "Likes", number: "200" },
+    { Name: "Users", number: users.length },
+    { Name: "Post", number: blog.length },
+    { Name: "Likes", number: blog.length },
     { Name: "Views", number: "245" },
   ];
 
-  const [blog, setBlog] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/posts");
-        // await new Promise((resolve) => setTimeout(resolve, 1000));
-        setBlog(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <div className="w-full grid place-items-center">
       <div className="w-[90%] mt-24">
@@ -42,23 +58,56 @@ const Dashboard = () => {
             );
           })}
         </div>
-        <div className="mt-20 h-[400px] ">
-          <UserChart />
-          <div className="text-white">
-            <h2>Latest Posts</h2>
-            <div className="text-white bg-cyan-200 text-xl h-[500px] w-[500px]">
-              <div className="h-[400px]">
+        <div className="mt-20 grid grid-cols-2 gap-10">
+
+          <div className="text-white border-2 border-cyan-200">
+            <h2 className="text-2xl text-center p-2">Latest Posts</h2>
+            <div className="text-white h-full w-full px-6 py-4">
+              <div className="h-2/4 w-full">
                 <img src={`http://localhost:3001/${blog.length > 0 && blog[blog.length - 1].image}`} alt="image" className="w-full h-full" />
               </div>
-              <div>
+              <div className="text-3xl font-semibold mt-4">
                 {blog.length > 0 && blog[blog.length - 1].title}
               </div>
-              <div>
-                {blog.length > 0 && blog[blog.length - 1].user}
+              <div className="text-sm text-slate-300 font-mono mt-2 capitalize flex justify-between">
+                <span>
+                  by {blog.length > 0 && blog[blog.length - 1].username}
+                </span>
+                <span>
+                  {blog.length > 0 && formatCreatedAt(blog[blog.length - 1].createdAt)}
+                </span>
               </div>
-              <div className="line-clamp-2">
+              <div className="line-clamp-4 font-mono mt-5">
                 {blog.length > 0 && blog[blog.length - 1].description}
               </div>
+            </div>
+          </div>
+
+          <div className="h-[100%] w-full px-14 pb-20 border-2 rounded-md border-cyan-200 p-2">
+            <h1 className="text-white text-xl p-4 text-center">List of Users</h1>
+            <div className="text-white">
+              <div className="flex justify-between text-lg">
+                <span>User Name</span>
+                <span>Joined At</span>
+              </div>
+              {
+                users.map((val, i) => {
+                  return (
+                    <div key={i}>
+                      <div className="flex justify-between py-2">
+                        <div className="flex">
+                          <span className="grid place-items-center text-2xl text-white font-semibold h-10 w-10 rounded-full m-1 p-1 capitalize bg-purple-600">
+                            {val.username.charAt(0)}
+                          </span>
+                          <div className="capitalize text-xl pt-2">{val.username}</div>
+                        </div>
+                        <span className='pt-2'>{formatCreatedAt(val.createdAt)}</span>
+                      </div>
+                      <hr />
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
